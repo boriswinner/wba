0dd047a610c6a8a141a1ced7df948a599402814c
boriswinner <timbori@mail.ru>
2017-11-09 21:37:32 +1000

    Added proper html templates and proper table output for table viewing

--- test_flask.py
+++ test_flask.py
@@ -1,56 +1,51 @@
 import fdb
 from flask import Flask
 from flask import render_template
+from flask import url_for
 from flask import request
 app = Flask(__name__)
 
 #constants
 GETTABLES = """SELECT a.RDB$RELATION_NAME
     FROM RDB$RELATIONS a
-    WHERE RDB$SYSTEM_FLAG = 0 AND RDB$RELATION_TYPE = 0
-        """
+    WHERE RDB$SYSTEM_FLAG = 0 AND RDB$RELATION_TYPE = 0"""
+GETCOLUMNNAMES = """select rdb$field_name 
+from rdb$relation_fields
+where rdb$relation_name= '%s'"""
 
 class dbConnection:
     connected = 0
+    gotTablesList = 0
     def connectToDatabase(self):
         if (not self.connected):
             self.con = fdb.connect(dsn='BASE.FDB', user='sysdba', password='masterkey', charset='UTF8')
             self.cur = self.con.cursor()
             self.connected = 1
+    def setTablesList(self):
+        if (not self.gotTablesList):
+            self.tablesList = self.cur.execute(GETTABLES).fetchall()
+            self.tablesList = [i[0] for i in self.tablesList]
+            self.gotTablesList = 1
 
 sheduleDB = dbConnection()
-
-def getTableColumns(table, cur):
-    query = """select rdb$field_name 
-from rdb$relation_fields
-where rdb$relation_name= '""" + table + "'"
-    cur.execute(query)
-    return cur.fetchall()
 
 @app.route("/", methods = ['GET'])
 def mainpage():
     sheduleDB.connectToDatabase()
-    cur = sheduleDB.cur
-    cur.execute(GETTABLES)
-    tables = cur.fetchall()
-    tablesList = []
-    for i in tables:
-        tablesList.append(str(i)[2:-4])
-    return  render_template("index.html") + render_template("picker.html", tables=tablesList)
+    sheduleDB.setTablesList()
+    return render_template("index.html", pickerName = 'tablesPicker', pickerURL = url_for('viewTable'),
+                           pickerElements = sheduleDB.tablesList)
 
 @app.route("/view_table", methods=['GET', 'POST'])
 def viewTable():
-    tableName = request.args.get("tablespicker")
-    header = "<h3>Viewing table: " + tableName + "</h3>"
     sheduleDB.connectToDatabase()
+    sheduleDB.setTablesList()
     cur = sheduleDB.cur
-    tableColumns = getTableColumns(tableName, cur)
+    tableName = request.args.get("tablesPicker")
+    tableColumns = cur.execute(GETCOLUMNNAMES % (tableName)).fetchall()
     query = "select * from " + tableName
     cur.execute(query)
     tableData = cur.fetchall()
-    tableDataString = ""
-    for i in tableColumns:
-        tableDataString += str(i) + '\r\n'
-    for i in tableData:
-        tableDataString += str(i) + '\r\n'
-    return mainpage() + header + '<pre>' + '\r\n' + tableDataString + '</pre>'
+    return render_template("tableView.html", tableName = tableName, columnNames = tableColumns, tableData = tableData,
+                           pickerName='tablesPicker', pickerURL = url_for('viewTable'),
+                           pickerElements = sheduleDB.tablesList)
