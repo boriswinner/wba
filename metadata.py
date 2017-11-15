f0c5fe54c83efce5731eb3540920e5eff42f7e7b
boriswinner <timbori@mail.ru>
2017-11-15 23:57:00 +1000

    Refactor

--- metadata.py
+++ metadata.py
@@ -4,30 +4,36 @@ class Meta:
         self.width = width
         self.name = name
 
+
 class BaseTable:
     def get_meta(self):
         result = {}
         for i in dir(self):
             if not callable(getattr(self, i)) and not i.startswith("__") and i.isupper():
-                result[i]=getattr(self,i)
+                result[i] = getattr(self, i)
         return result
 
+
 class Audiences(BaseTable):
-    ID = Meta('int','1','id')
-    NAME = Meta('string','2','Номер аудитории')
+    ID = Meta('int', '1', 'id')
+    NAME = Meta('string', '2', 'Номер аудитории')
+
 
 class Lessons(BaseTable):
-    ID = Meta('int','4','id')
-    NAME = Meta('string','8','Название занятия')
+    ID = Meta('int', '4', 'id')
+    NAME = Meta('string', '8', 'Название занятия')
     ORDER_NUMBER = Meta('int', '4', 'id')
 
+
 class Groups(BaseTable):
-    ID = Meta('int','1','id')
-    NAME = Meta('string','2','Номер группы')
+    ID = Meta('int', '1', 'id')
+    NAME = Meta('string', '2', 'Номер группы')
+
 
 class LessonTypes(BaseTable):
-    ID = Meta('int','1','id')
-    NAME = Meta('string','2','Тип занятия')
+    ID = Meta('int', '1', 'id')
+    NAME = Meta('string', '2', 'Тип занятия')
+
 
 audiences = Audiences()
 lessons = Lessons()
--- server.py
+++ server.py
@@ -4,10 +4,11 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import request
+
 app = Flask(__name__)
 import sys
 
-#constants
+# constants
 GETTABLES = """SELECT a.RDB$RELATION_NAME
     FROM RDB$RELATIONS a
     WHERE RDB$SYSTEM_FLAG = 0 AND RDB$RELATION_TYPE = 0"""
@@ -15,45 +16,48 @@ GETCOLUMNNAMES = """select rdb$field_name
 from rdb$relation_fields
 where rdb$relation_name= '%s'"""
 
+
 class DbConnection:
     connected = 0
     gotTablesList = 0
-    def connectToDatabase(self):
+
+    def connect_to_database(self):
         if (not self.connected):
             self.con = fdb.connect(dsn='BASE.FDB', user='sysdba', password='masterkey', charset='UTF8')
             self.cur = self.con.cursor()
             self.connected = 1
-    def setTablesList(self):
+
+    def set_tables_list(self):
         if (not self.gotTablesList):
             self.tablesList = self.cur.execute(GETTABLES).fetchall()
             self.tablesList = [i[0] for i in self.tablesList]
             self.gotTablesList = 1
 
-sheduleDB = DbConnection()
 
-@app.route("/", methods = ['GET'])
+scheduleDB = DbConnection()
+
+
+@app.route("/", methods=['GET'])
 def mainpage():
-    sheduleDB.connectToDatabase()
-    sheduleDB.setTablesList()
-    return render_template("tableView.html", pickerName = 'tablesPicker', pickerURL = url_for('viewTable'),
-                           pickerElements = sheduleDB.tablesList)
+    scheduleDB.connect_to_database()
+    scheduleDB.set_tables_list()
+    return render_template("tableView.html", pickerName='tablesPicker', pickerURL=url_for('view_table'),
+                           pickerElements=scheduleDB.tablesList)
+
 
 @app.route("/view_table", methods=['GET', 'POST'])
-def viewTable():
-    sheduleDB.connectToDatabase()
-    sheduleDB.setTablesList()
-    cur = sheduleDB.cur
+def view_table():
+    scheduleDB.connect_to_database()
+    scheduleDB.set_tables_list()
+    cur = scheduleDB.cur
     tableName = request.args.get("tablesPicker")
     t = getattr(metadata, tableName.lower())
     meta = t.get_meta()
-    print(meta)
     tableColumns = cur.execute(GETCOLUMNNAMES % (tableName)).fetchall()
     tableColumns = [str(i[0]).strip() for i in tableColumns]
     query = "select * from " + tableName
     cur.execute(query)
     tableData = cur.fetchall()
-    print(tableColumns)
-    print(tableData)
-    return render_template("tableView.html", tableName = tableName, columnNames = tableColumns, tableData = tableData,
-                           pickerName='tablesPicker', pickerURL = url_for('viewTable'),
-                           pickerElements = sheduleDB.tablesList, meta = meta)
+    return render_template("tableView.html", tableName=tableName, columnNames=tableColumns, tableData=tableData,
+                           pickerName='tablesPicker', pickerURL=url_for('view_table'),
+                           pickerElements=scheduleDB.tablesList, meta=meta)
